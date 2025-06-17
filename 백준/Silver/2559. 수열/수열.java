9edc09df204339db4c1829a4a464c6e90686eaae
Lee, Nam Gon <sharkslee0@gmail.com>
2025-06-17 22:47:29 +0900

    [Silver III] Title: 수열, Time: 244 ms, Memory: 24016 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -18,8 +18,8 @@ public class Main {
     for (int end = k; end < n + 1; end++) {
       answer = Math.max(answer, prefixSum[end] - prefixSum[end - k]);
     }
-    sb.append(answer);
 
+    sb.append(answer);
     output();
   }
 
@@ -30,8 +30,8 @@ public class Main {
     k = Integer.parseInt(st.nextToken());
     prefixSum = new int[n + 1];
     st = new StringTokenizer(br.readLine());
-    for (int i = 1; i < n + 1; i++) {
-      prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(st.nextToken());
+    for (int i = 0; i < n; i++) {
+      prefixSum[i + 1] = prefixSum[i] + Integer.parseInt(st.nextToken());
     }
   }
 
