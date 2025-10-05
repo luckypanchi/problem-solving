a4225a0310c6fbfd4c0a705ac1494378baf3438f
Lee, Nam Gon <sharkslee0@gmail.com>
2025-10-06 00:59:55 +0900

    [Gold V] Title: 보물섬, Time: 476 ms, Memory: 191044 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,84 +1,108 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
-
-class Node {
-
-  int x, y;
-  int count;
-
-  public Node(int x, int y, int count) {
-    this.x = x;
-    this.y = y;
-    this.count = count;
-  }
-}
 
 public class Main {
 
-  static int r, c;
-  static boolean[][] visited;
-  static char[][] matrix;
-  static int answer = Integer.MIN_VALUE;
-  static int[] dx = { 1, 0, -1, 0 };
-  static int[] dy = { 0, -1, 0, 1 };
+  static StringBuilder sb = new StringBuilder();
+  static int n, m;
+  static char[][] board;
+  static int[] dx = {1, 0, -1, 0};
+  static int[] dy = {0, 1, 0, -1};
+  static int[][] visited;
+  static Deque<Node> que;
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    r = Integer.parseInt(st.nextToken());
-    c = Integer.parseInt(st.nextToken());
-
-    matrix = new char[r][c];
-
-    for (int i = 0; i < r; i++) {
-      String row = br.readLine();
-      for (int j = 0; j < c; j++) {
-        matrix[i][j] = row.charAt(j);
-      }
-    }
-
-    for (int i = 0; i < r; i++) {
-      for (int j = 0; j < c; j++) {
-        if (matrix[i][j] == 'L') {
-          bfs(i, j);
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (board[i][j] == 'L') {
+          answer = Math.max(answer, bfs(i, j));
         }
       }
     }
-    System.out.println(answer);
+
+    sb.append(answer);
+    output();
   }
 
-  public static void bfs(int y, int x) {
-    Queue<Node> que = new LinkedList<>();
-    visited = new boolean[r][c];
+  private static int bfs(int startY, int startX) {
+    visited = new int[n][m];
+    visited[startY][startX] = 1;
 
-    que.add(new Node(x, y, 0));
-    visited[y][x] = true;
+    que = new ArrayDeque<>();
+    que.add(new Node(startY, startX));
+
+    int max = 0;
 
     while (!que.isEmpty()) {
-      Node node = que.poll();
-
-      answer = Math.max(answer, node.count);
+      Node curr = que.poll();
+      max = Math.max(max, visited[curr.y][curr.x]);
 
       for (int i = 0; i < 4; i++) {
-        int nx = node.x + dx[i];
-        int ny = node.y + dy[i];
+        int ny = curr.y + dy[i];
+        int nx = curr.x + dx[i];
 
-        if (
-          0 <= nx &&
-          nx < c &&
-          0 <= ny &&
-          ny < r &&
-          matrix[ny][nx] == 'L' &&
-          visited[ny][nx] == false
-        ) {
-          visited[ny][nx] = true;
-          que.add(new Node(nx, ny, node.count + 1));
+        if (!checkRange(ny, nx) || board[ny][nx] != 'L' || visited[ny][nx] != 0) {
+          continue;
         }
+
+        visited[ny][nx] = visited[curr.y][curr.x] + 1;
+        que.add(new Node(ny, nx));
+      }
+    }
+
+    return max - 1;
+  }
+
+  private static boolean checkRange(int y, int x) {
+    return 0 <= y && y < n && 0 <= x && x < m;
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    board = new char[n][m];
+    for (int i = 0; i < n; i++) {
+      String line = br.readLine();
+      for (int j = 0; j < m; j++) {
+        board[i][j] = line.charAt(j);
       }
     }
   }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  private static class Node {
+
+    int y, x;
+
+    public Node(int y, int x) {
+      this.y = y;
+      this.x = x;
+    }
+
+    @Override
+    public String toString() {
+      return "Node{" +
+          "y=" + y +
+          ", x=" + x +
+          '}';
+    }
+  }
+
 }
