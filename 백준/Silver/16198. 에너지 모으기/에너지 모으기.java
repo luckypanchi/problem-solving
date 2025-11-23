4d6fcb9d49cca27006cdd20b6b47e8e2fd53402c
Lee, Nam Gon <sharkslee0@gmail.com>
2025-11-23 15:40:33 +0900

    [Silver I] Title: 에너지 모으기, Time: 128 ms, Memory: 15696 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
@@ -3,38 +3,60 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
   static StringBuilder sb = new StringBuilder();
   static int n;
-  static List<Integer> numbers;
+  static int[] weights;
+  static boolean[] isUsed;
 
   public static void main(String[] args) throws IOException {
     setUp();
 
-    sb.append(solve(0));
-
+    sb.append(solve(n));
     output();
   }
 
-  private static int solve(int prevSum) {
-    if (numbers.size() == 2) {
-      return prevSum;
+  private static int solve(int totalCount) {
+    if (totalCount == 2) {
+      return 0;
     }
 
-    int result = Integer.MIN_VALUE;
-    for (int i = 1; i < numbers.size() - 1; i++) {
-      int acc = numbers.get(i - 1) * numbers.get(i + 1);
-      int target = numbers.remove(i);
-      result = Math.max(result, solve(prevSum + acc));
-      numbers.add(i, target);
+    int result = 0;
+
+    for (int i = 1; i < n - 1; i++) {
+      if (isUsed[i]) {
+        continue;
+      }
+
+      isUsed[i] = true;
+      int currResult = findLeft(i) * findRight(i) + solve(totalCount - 1);
+      isUsed[i] = false;
+
+      result = Math.max(result, currResult);
     }
 
     return result;
+  }
+
+  private static int findLeft(int start) {
+    for (int i = start - 1; i >= 0; i--) {
+      if (!isUsed[i]) {
+        return weights[i];
+      }
+    }
+    return -1;
+  }
+
+  private static int findRight(int start) {
+    for (int i = start + 1; i < n; i++) {
+      if (!isUsed[i]) {
+        return weights[i];
+      }
+    }
+    return -1;
   }
 
   private static void setUp() throws IOException {
@@ -42,10 +64,11 @@ public class Main {
     StringTokenizer st;
 
     n = Integer.parseInt(br.readLine());
-    numbers = new ArrayList<>();
+    weights = new int[n];
+    isUsed = new boolean[n];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
-      numbers.add(Integer.parseInt(st.nextToken()));
+      weights[i] = Integer.parseInt(st.nextToken());
     }
   }
 
