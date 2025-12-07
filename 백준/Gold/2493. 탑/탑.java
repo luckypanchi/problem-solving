7d00d6387548efed3561c8d0ffa0019c63b52560
Lee, Nam Gon <sharkslee0@gmail.com>
2025-12-07 20:57:33 +0900

    [Gold V] Title: 탑, Time: 660 ms, Memory: 84720 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,44 +1,66 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+  static StringBuilder sb = new StringBuilder();
+  static int n;
+  static int[] towers;
+  static int[] answer;
 
-		int n = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		int[] numbers = new int[n];
-		for (int i = 0; i < n; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
-		}
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-		int[] answer = new int[n];
-		Stack<Integer> stack = new Stack<>();
-		stack.push(0);
-		for (int i = 1; i < n; i++) {
-			while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
-				stack.pop();
-			}
+    Deque<Integer> stack = new ArrayDeque<>();
 
-			if (!stack.isEmpty()) {
-				answer[i] = stack.peek() + 1;
-			}
-			stack.push(i);
+    for (int towerNumber = n; towerNumber > 0; towerNumber--) {
+      if (stack.isEmpty()) {
+        stack.push(towerNumber);
+        continue;
+      }
 
-		}
+      if (towers[stack.peek()] < towers[towerNumber]) {
+        while (!stack.isEmpty() && towers[stack.peek()] <= towers[towerNumber]) {
+          answer[stack.pop()] = towerNumber;
+        }
+      }
+      
+      stack.push(towerNumber);
+    }
 
-		for (int i : answer) {
-			sb.append(i).append(" ");
-		}
+    while (!stack.isEmpty()) {
+      answer[stack.pop()] = 0;
+    }
 
-		System.out.println(sb);
+    for (int i = 1; i < n + 1; i++) {
+      sb.append(answer[i]).append(" ");
+    }
+    output();
+  }
 
-	}
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    n = Integer.parseInt(br.readLine());
+    towers = new int[n + 1];
+    answer = new int[n + 1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      towers[i + 1] = Integer.parseInt(st.nextToken());
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
 
 }
