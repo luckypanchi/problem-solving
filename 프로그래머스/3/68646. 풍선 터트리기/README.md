1a46ea6b6211dc43182c02befd1be0195339ea1b
Lee, Nam Gon <sharkslee0@gmail.com>
2025-12-18 22:37:56 +0900

    [level 3] Title: 풍선 터트리기, Time: 33.41 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/README.md
+++ 프로그래머스/3/68646. 풍선 터트리기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 풍선 터트리기 - 68646 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68646?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68646) 
 
 ### 성능 요약
 
-메모리: 142 MB, 시간: 30.91 ms
+메모리: 146 MB, 시간: 33.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 16:16:05
+2025년 12월 18일 22:37:50
 
 ### 문제 설명
 
--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,32 +1,29 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int[] a) {
-        if (a.length == 1) {
-            return 1;
-        }
-        if (a.length == 2) {
-            return 2;
-        }
+        int length = a.length;
+        int[] leftMin = new int[length];
+        int[] rightMin = new int[length];
         
-        int[] leftMin = new int[a.length];
-        int[] rightMin = new int[a.length];
         leftMin[0] = a[0];
-        rightMin[a.length - 1] = a[a.length - 1];
-        for (int i = 1; i < a.length; i++) {
+        for (int i = 1; i < length; i++) {
             leftMin[i] = Math.min(leftMin[i - 1], a[i]);
         }
-        for (int i = a.length - 2; i > -1; i--) {
+        
+        rightMin[length - 1] = a[length - 1];
+        for (int i = length - 2; i > -1; i--) {
             rightMin[i] = Math.min(rightMin[i + 1], a[i]);
         }
         
-        int answer = 2;
-        for (int i = 1; i < a.length - 1; i++) {
-            if (leftMin[i] >= a[i] || a[i] <= rightMin[i]) {
+        int answer = 0;
+        for (int i = 0; i < length; i++) {
+            if (a[i] <= leftMin[i] || a[i] <= rightMin[i]) {
                 answer++;
             }
         }
-       
+        
         return answer;
     }
 }
