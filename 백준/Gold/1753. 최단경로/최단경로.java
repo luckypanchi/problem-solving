4126ae7416588006ba08f7c8da400ba6dcaa81c3
Lee, Nam Gon <sharkslee0@gmail.com>
2025-12-25 18:04:35 +0900

    [Gold IV] Title: 최단경로, Time: 740 ms, Memory: 116324 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,73 +13,97 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int v, e, k;
-	static int[] results;
-	static PriorityQueue<Node> priorityQueue;
-	static Map<Integer, List<Node>> graph;
+  static StringBuilder sb = new StringBuilder();
+  static int v, e;
+  static int start;
+  static Map<Integer, List<Edge>> map;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(br.readLine());
-		results = new int[v + 1];
-		for (int i = 0; i < v + 1; i++) {
-			results[i] = Integer.MAX_VALUE;
-		}
-		graph = new HashMap<>();
-		for (int i = 1; i < v + 1; i++) {
-			graph.put(i, new ArrayList<>());
-		}
-		for (int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			graph.get(u).add(new Node(v, w));
-		}
-		priorityQueue = new PriorityQueue<>();
-		priorityQueue.add(new Node(k, 0));
-		results[k] = 0;
-		while (!priorityQueue.isEmpty()) {
-			Node node = priorityQueue.poll();
-			if (node.cost <= results[node.nodeNumber]) {
-				for (Node nextNode : graph.get(node.nodeNumber)) {
-					int nextCost = node.cost + nextNode.cost;
-					if (nextCost < results[nextNode.nodeNumber]) {
-						results[nextNode.nodeNumber] = nextCost;
-						priorityQueue.add(new Node(nextNode.nodeNumber, nextCost));
-					}
-				}
-			}
-		}
-		for (int i = 1; i < v + 1; i++) {
-			if (results[i] == Integer.MAX_VALUE) {
-				sb.append("INF");
-			} else {
-				sb.append(results[i]);
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-	static class Node implements Comparable<Node> {
+    int[] distances = dijkstra();
 
-		int cost;
-		int nodeNumber;
+    for (int i = 1; i < v + 1; i++) {
+      sb.append(distances[i] != Integer.MAX_VALUE ? distances[i] : "INF").append("\n");
+    }
 
-		public Node(int nodeNumber, int cost) {
-			this.nodeNumber = nodeNumber;
-			this.cost = cost;
-		}
+    output();
+  }
 
-		@Override
-		public int compareTo(Node other) {
-			return this.cost - other.cost;
-		}
-	}
+  private static int[] dijkstra() {
+    int[] distances = new int[v + 1];
+    Arrays.fill(distances, Integer.MAX_VALUE);
+    distances[start] = 0;
+
+    PriorityQueue<Pair> heap = new PriorityQueue<>((o1, o2) -> o1.totalWeight - o2.totalWeight);
+    heap.add(new Pair(start, distances[start]));
+
+    while (!heap.isEmpty()) {
+      Pair curr = heap.poll();
+
+      if (distances[curr.node] < curr.totalWeight) {
+        continue;
+      }
+
+      for (Edge next : map.get(curr.node)) {
+        int totalWeight = curr.totalWeight + next.weight;
+        if (totalWeight < distances[next.dest]) {
+          distances[next.dest] = totalWeight;
+          heap.add(new Pair(next.dest, totalWeight));
+        }
+      }
+    }
+
+    return distances;
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    v = Integer.parseInt(st.nextToken());
+    e = Integer.parseInt(st.nextToken());
+    start = Integer.parseInt(br.readLine());
+    map = new HashMap<>();
+    for (int i = 0; i < v + 1; i++) {
+      map.put(i, new ArrayList<>());
+    }
+
+    for (int i = 0; i < e; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
+      map.get(u).add(new Edge(v, w));
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  private static class Pair {
+
+    int node;
+    int totalWeight;
+
+    public Pair(int node, int totalWeight) {
+      this.node = node;
+      this.totalWeight = totalWeight;
+    }
+  }
+
+  private static class Edge {
+
+    int dest;
+    int weight;
+
+    public Edge(int dest, int weight) {
+      this.dest = dest;
+      this.weight = weight;
+    }
+  }
 
 }
