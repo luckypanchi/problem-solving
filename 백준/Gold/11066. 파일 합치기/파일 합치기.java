3804507986dcfe1d4fb70a083b700c8f403bdf67
Lee, Nam Gon <sharkslee0@gmail.com>
2025-12-27 23:21:38 +0900

    [Gold III] Title: 파일 합치기, Time: 392 ms, Memory: 27968 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,47 +1,58 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int tcs, k;
-    static int[] cost;
-    static int[][] dp;
+  static StringBuilder sb = new StringBuilder();
+  static int k;
+  static int[] prefixSum;
+  static int[][] dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-        tcs = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < tcs; tc++) {
-            k = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            cost = new int[k + 1];
-            for (int i = 1; i < k + 1; i++) {
-                cost[i] = cost[i - 1] + Integer.parseInt(st.nextToken());
-            }
+    int testcases = Integer.parseInt(br.readLine());
 
-            dp = new int[k + 1][k + 1];
-            for (int length = 1; length < k + 1; length++) {
-                for (int start = 1; start < k - length + 1; start++) {
-                    int end = start + length;
-                    dp[start][end] = getMinCost(start, end, length) + (cost[end] - cost[start - 1]);
-                }
-            }
-
-            sb.append(dp[1][k]).append("\n");
+    while (testcases-- > 0) {
+      k = Integer.parseInt(br.readLine());
+      prefixSum = new int[k + 1];
+      st = new StringTokenizer(br.readLine());
+      for (int i = 1; i < k + 1; i++) {
+        int chapter = Integer.parseInt(st.nextToken());
+        prefixSum[i] += prefixSum[i - 1] + chapter;
+      }
+      dp = new int[k + 1][k + 1];
+      for (int length = 1; length < k + 1; length++) {
+        for (int start = 1; start + length < k + 1; start++) {
+          int end = start + length;
+          dp[start][end] = getMinCost(start, end, length) + prefixSum[end] - prefixSum[start - 1];
         }
+      }
 
-        System.out.println(sb);
+      sb.append(dp[1][k]).append("\n");
     }
 
-    private static int getMinCost(int start, int end, int length) {
-        int result = Integer.MAX_VALUE;
-        for (int mid = 0; mid < length; mid++) {
-            result = Math.min(result, dp[start][start + mid] + dp[start + mid + 1][end]);
-        }
-        return result;
+    output();
+  }
+
+  private static int getMinCost(int start, int end, int length) {
+    int result = Integer.MAX_VALUE;
+    for (int len = 0; len < length; len++) {
+      result = Math.min(result, dp[start][start + len] + dp[start + len + 1][end]);
     }
+    return result;
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
