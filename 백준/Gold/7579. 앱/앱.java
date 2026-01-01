f13a0da576d189c23c81e687d735c1778033f074
Lee, Nam Gon <sharkslee0@gmail.com>
2026-01-01 20:53:37 +0900

    [Gold III] Title: 앱, Time: 140 ms, Memory: 19920 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -3,32 +3,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
   static StringBuilder sb = new StringBuilder();
   static int n, m;
-  static int[] memorySizes;
+  static int[] memories;
   static int[] costs;
 
   public static void main(String[] args) throws IOException {
     setUp();
 
-    int totalCost = Arrays.stream(costs).sum();
+    int totalCost = sum(costs);
     int[][] dp = new int[n + 1][totalCost + 1];
     int answer = Integer.MAX_VALUE;
 
-    for (int stuff = 1; stuff < n + 1; stuff++) {
+    for (int i = 1; i < n + 1; i++) {
       for (int currCost = 0; currCost < totalCost + 1; currCost++) {
-        if (costs[stuff] <= currCost) {
-          dp[stuff][currCost] = Math.max(dp[stuff - 1][currCost], memorySizes[stuff] + dp[stuff - 1][currCost - costs[stuff]]);
+        if (costs[i] <= currCost) {
+          dp[i][currCost] = Math.max(dp[i - 1][currCost], dp[i - 1][currCost - costs[i]] + memories[i]);
         } else {
-          dp[stuff][currCost] = dp[stuff - 1][currCost];
+          dp[i][currCost] = dp[i - 1][currCost];
         }
 
-        if (m <= dp[stuff][currCost]) {
+        if (m <= dp[i][currCost]) {
           answer = Math.min(answer, currCost);
         }
       }
@@ -38,16 +37,24 @@ public class Main {
     output();
   }
 
+  private static int sum(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  }
+
   private static void setUp() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     n = Integer.parseInt(st.nextToken());
     m = Integer.parseInt(st.nextToken());
-    memorySizes = new int[n + 1];
+    memories = new int[n + 1];
     costs = new int[n + 1];
     st = new StringTokenizer(br.readLine());
     for (int i = 1; i < n + 1; i++) {
-      memorySizes[i] = Integer.parseInt(st.nextToken());
+      memories[i] = Integer.parseInt(st.nextToken());
     }
     st = new StringTokenizer(br.readLine());
     for (int i = 1; i < n + 1; i++) {
